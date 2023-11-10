b93fe52e1de7b97b75959d8e33fadc3f0d1b0747
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-10 09:09:05 +0000

    1

--- scrap.py
+++ scrap.py
@@ -2,36 +2,31 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-# Function to scrape product data from Shopee for a specific location using requests and BeautifulSoup
+# Function to scrape product data from Shopee for a specific location
 def scrape_shopee_products(keyword, location):
     # Define the URL with the specified keyword and location
     url = f'https://shopee.co.id/search?keyword={keyword}&locations={location}&noCorrection=true&page=0'
-
-    # Send an HTTP GET request to the URL
+    #https://shopee.co.id/search?keyword=sepatu&locations=Riau&noCorrection=true&page=0
+    # Send an HTTP GET request to Shopee
     response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content of the page
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Extract product details
-        product_list = []
-
-        for product in soup.find_all('div', class_='product'):
-            product_details = {
-                'name': product.find('div', class_='product-name').text,
-                'price': product.find('div', class_='product-price').text,
-            }
-
-            # You can add more fields as needed
-
-            product_list.append(product_details)
-
-        return product_list
-
-    else:
-        st.error('Failed to retrieve data. Please check the URL or try again later.')
+    
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Extract product details
+    product_list = []
+    
+    for product in soup.find_all('div', class_='product'):
+        product_details = {
+            'name': product.find('div', class_='product-name').text,
+            'price': product.find('div', class_='product-price').text,
+        }
+        
+        # You can add more fields as needed
+        
+        product_list.append(product_details)
+    
+    return product_list
 
 # Streamlit app
 def main():
@@ -42,9 +37,8 @@ def main():
     if st.button('Scrape Products'):
         if keyword and location:
             products = scrape_shopee_products(keyword, location)
-            if products:
-                st.write('Scraped Product Data:')
-                st.write(products)
+            st.write('Scraped Product Data:')
+            st.write(products)
         else:
             st.warning('Please enter a keyword and location.')
 
