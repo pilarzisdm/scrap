8395021cf47112590eeca372d897f6261f6bf40e
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-10 04:17:17 +0000

    y

--- scrap.py
+++ scrap.py
@@ -1,48 +1,37 @@
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 from bs4 import BeautifulSoup
 
-# Function to scrape product data from Shopee for a specific location using Selenium
+# Function to scrape product data from Shopee for a specific location using requests and BeautifulSoup
 def scrape_shopee_products(keyword, location):
     # Define the URL with the specified keyword and location
     url = f'https://shopee.co.id/search?keyword={keyword}&locations={location}&noCorrection=true&page=0'
 
-    # Create a Selenium WebDriver instance (you need to specify the path to your web driver)
-    driver = webdriver.Chrome(executable_path='C:\Program Files\Google\Chrome\Application\chromedriver.exe')
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
 
-    # Open the URL in the browser
-    driver.get(url)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Wait for the page to load (you might need to adjust the wait time)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'product')))
+        # Extract product details
+        product_list = []
 
-    # Get the page source after it has fully loaded
-    page_source = driver.page_source
+        for product in soup.find_all('div', class_='product'):
+            product_details = {
+                'name': product.find('div', class_='product-name').text,
+                'price': product.find('div', class_='product-price').text,
+            }
 
-    # Close the browser
-    driver.quit()
+            # You can add more fields as needed
 
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(page_source, 'html.parser')
+            product_list.append(product_details)
 
-    # Extract product details
-    product_list = []
+        return product_list
 
-    for product in soup.find_all('div', class_='product'):
-        product_details = {
-            'name': product.find('div', class_='product-name').text,
-            'price': product.find('div', class_='product-price').text,
-        }
-
-        # You can add more fields as needed
-
-        product_list.append(product_details)
-
-    return product_list
+    else:
+        st.error('Failed to retrieve data. Please check the URL or try again later.')
 
 # Streamlit app
 def main():
@@ -53,8 +42,9 @@ def main():
     if st.button('Scrape Products'):
         if keyword and location:
             products = scrape_shopee_products(keyword, location)
-            st.write('Scraped Product Data:')
-            st.write(products)
+            if products:
+                st.write('Scraped Product Data:')
+                st.write(products)
         else:
             st.warning('Please enter a keyword and location.')
 
