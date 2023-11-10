13ab39ebda036b8d18c7fe4a1c4444f6d3ae0601
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-10 03:24:51 +0000

    y

--- scrap.py
+++ scrap.py
@@ -1,33 +1,46 @@
+import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-# Define the URL of the e-commerce website's shop listings for a specific location
-url = 'https://www.example.com/shops?location=your_location'
-
-# Send an HTTP GET request to the website
-response = requests.get(url)
-
-# Parse the HTML content of the page
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Extract shop details
-shop_list = []
-
-for shop in soup.find_all('div', class_='shop'):
-    shop_details = {
-        'name': shop.find('h3').text,
-        'address': shop.find('p', class_='address').text,
-        'description': shop.find('p', class_='description').text,
-    }
+# Function to scrape shop data
+def scrape_shops(location):
+    # Define the URL with the specified location
+    url = f'https://www.shopee.co.id/shops?location={location}'
     
-    # Check if the shop is in the desired location
-    if 'your_location' in shop_details['address']:
-        shop_list.append(shop_details)
+    # Send an HTTP GET request to the website
+    response = requests.get(url)
+    
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extract shop details
+    shop_list = []
+    
+    for shop in soup.find_all('div', class_='shop'):
+        shop_details = {
+            'name': shop.find('h3').text,
+            'address': shop.find('p', class_='address').text,
+            'description': shop.find('p', class_='description').text,
+        }
+        
+        # Check if the shop is in the desired location
+        if location in shop_details['address']:
+            shop_list.append(shop_details)
+    
+    return shop_list
 
-# You can then store or display the shop data as needed
-import streamlit as st
+# Streamlit app
+def main():
+    st.title('Web Scrap Online Shop E-Commerce')
+    location = st.text_input('Masukan lokasi:')
+    
+    if st.button('Scrape Shops'):
+        if location:
+            shops = scrape_shops(location)
+            st.write('Scraped Shop Data:')
+            st.write(shops)
+        else:
+            st.warning('Please enter a location.')
 
-# Display the data using Streamlit
-st.title('E-commerce Product Scraper')
-st.write('Scraped Product Data:')
-st.write(product_list)
+if __name__ == '__main__':
+    main()
